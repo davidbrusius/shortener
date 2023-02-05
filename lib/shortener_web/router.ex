2ae353f00db9189a0b0d0dbcf1fe1309430cffdf
David Brusius <brusiusdavid@gmail.com>
2023-02-05 20:28:08 -0300

    Generate Phoenix app
    For reference, the Phoenix app was generated using the following command:
    
    ```
    $ mix phx.new . --module UScore --no-assets --no-html --no-gettext --no-dashboard --no-live --no-mailer --verbose
    ```
    
    Some files were removed to cleanup the generated code.

---
+++ lib/shortener_web/router.ex
@@ -0,0 +1,16 @@
+defmodule ShortenerWeb.Router do
+  use ShortenerWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, {ShortenerWeb.LayoutView, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  scope "/", ShortenerWeb do
+    pipe_through :browser
+  end
+end
